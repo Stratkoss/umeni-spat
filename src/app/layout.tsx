e1d24ffa3a4a68b0a4743a9deb103fe65514e9bb
David Stratil <david23stratil@gmail.com>
2026-09-04 18:51:15 +0200

    Declare smooth scroll behavior

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,5 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="cs"><body>{children}</body></html>;
+  return <html lang="cs" data-scroll-behavior="smooth"><body>{children}</body></html>;
 }
